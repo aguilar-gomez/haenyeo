1b2a29fb8956505b87fded0e59237d63c8c4b93e
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2022-08-19 14:34:29 -0700

    Create selscan.sh

---
+++ selscan.sh
@@ -0,0 +1,15 @@
+#Generate matrices with value added
+for k in $(seq 0 7);
+do
+cmatrixK.py c.matrix_k8 $k 10
+done
+ 
+#Run qpas to calculate allele frequency matrix for all positions using the inferred components with k=7
+$OHANA/qpas haenyeov12.dgm -k 7 -qi heanyeo_v11_k8_e0.08_mi450_q.matrix -fo haenyeov12_k7.full.f.matrix -e 0.08 -mi 450 >out.qpas.full &
+
+#Run selscan
+for k in $(seq 0 6);
+do 
+echo "running selscan with selection in component k $k"
+$OHANA/selscan haenyeov12.dgm haenyeov12_k7.full.f.matrix c.matrix_k7 -cs c.matrix_k7selK${k}_h10 > scan.haenyeok$k.txt &
+done
