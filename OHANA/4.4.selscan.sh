16babeeeda9fd919ebeef4a64a2291ac8bda7841
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2022-11-21 14:22:18 -0800

    Update and rename selscan.sh to 4.4.selscan.sh

--- OHANA/4.4.selscan.sh
+++ OHANA/4.4.selscan.sh
@@ -5,11 +5,11 @@ cmatrixK.py c.matrix_k8 $k 10
 done
  
 #Run qpas to calculate allele frequency matrix for all positions using the inferred components with k=7
-$OHANA/qpas haenyeov12.dgm -k 7 -qi heanyeo_v11_k8_e0.08_mi450_q.matrix -fo haenyeov12_k7.full.f.matrix -e 0.08 -mi 450 >out.qpas.full &
+$OHANA/qpas haenyeov13.dgm -k 7 -qi heanyeo_v13_k8_e0.08_mi450_q.matrix -fo haenyeov13_k7.full.f.matrix -e 0.08 -mi 450 >out.qpas.full &
 
 #Run selscan
 for k in $(seq 0 6);
 do 
 echo "running selscan with selection in component k $k"
-$OHANA/selscan haenyeov12.dgm haenyeov12_k7.full.f.matrix c.matrix_k7 -cs c.matrix_k7selK${k}_h10 > scan.haenyeok$k.txt &
+$OHANA/selscan haenyeov13.dgm haenyeov13_k7.full.f.matrix c.matrix_k7 -cs c.matrix_k7selK${k}_h10 > scan.haenyeok$k.txt &
 done
