e46bbbfff0d6c8e347068723cfa3b3687ea54a35
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2022-11-21 13:20:30 -0800

    Update and rename nemeco.sh to 4.3.nemeco.sh

---
+++ OHANA/4.3.nemeco.sh
@@ -0,0 +1,10 @@
+Kmin=3 # min value of k
+Kmax=9 # maximum value of k
+
+for k in $(seq $Kmin $Kmax);
+do
+echo "running nemeco with k $k"
+$OHANA/nemeco haenyeov133percent.dgm heanyeo_v13_k${k}_e${e}_mi${mi}_f.matrix -co c.matrix_k${k} -mi 5 > out1.nemk${k} 
+$OHANA/convert cov2nwk c.matrix_k${k} haenyeov13_k${k}.nwk
+tail -n +2 heanyeo_v13_k${k}_e${e}_mi${mi}_q.matrix  > haenyeov13_k${k}.Q
+done
