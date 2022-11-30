f1344528b68c853f79db599e1e36a05ee1308f72
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2022-11-30 13:07:27 -0800

    Update 4.3.nemeco.sh

--- 4.OHANA/4.3.nemeco.sh
+++ 4.OHANA/4.3.nemeco.sh
@@ -1,12 +1,12 @@
-Kmin=3 # min value of k
-Kmax=9 # maximum value of k
+Kmin=4 # min value of k
+Kmax=8 # maximum value of k
 mi=450 # max number of iterations
 e=0.08 # minimum delta for likelihood
 
 for k in $(seq $Kmin $Kmax);
 do
 echo "running nemeco with k $k"
-$OHANA/nemeco haenyeov133percent.dgm heanyeo_v13_k${k}_e${e}_mi${mi}_f.matrix -co c.matrix_k${k} -mi 5 > out1.nemk${k} 
-$OHANA/convert cov2nwk c.matrix_k${k} haenyeov13_k${k}.nwk
-tail -n +2 heanyeo_v13_k${k}_e${e}_mi${mi}_q.matrix  > haenyeov13_k${k}.Q
+$OHANA/nemeco haenyeov143percent.dgm heanyeo_v14_k${k}_e${e}_mi${mi}_f.matrix -co c.matrix_k${k} -mi 5 > out1.nemk${k} 
+$OHANA/convert cov2nwk c.matrix_k${k} haenyeov14_k${k}.nwk
+tail -n +2 heanyeo_v14_k${k}_e${e}_mi${mi}_q.matrix  > haenyeov14_k${k}.Q
 done
