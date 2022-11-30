a0108e111ef447064809fdc87d9b35191c4153ad
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2022-11-30 12:15:59 -0800

    Update 4.1.convert.sh

--- 4.OHANA/4.1.convert.sh
+++ 4.OHANA/4.1.convert.sh
@@ -1,14 +1,13 @@
 OHANA=$PATHTOOHANA
 
 #Recode with plink 
-plink --bfile allchr.version13 --recode 12 tab --out haenyeo.forohanav13 
+plink --bfile allchr.version14 --recode 12 tab --out haenyeo.forohanav14
 
 #Convert plink format to OHANA
-$OHANA/convert ped2dgm haenyeo.forohanav11.ped haenyeov11.dgm 
-$OHANA/convert ped2dgm haenyeo.forohanav13.ped haenyeov13.dgm 
+$OHANA/convert ped2dgm haenyeo.forohanav14.ped haenyeov14.dgm 
 
 #Sample sites for OHANA qpas (in this case 3%)
-$OHANA/sample-sites.py haenyeov13.dgm  3 ./haenyeov133percent.dgm
+$OHANA/sample-sites.py haenyeov14.dgm  3 ./haenyeov143percent.dgm
 
 
 
