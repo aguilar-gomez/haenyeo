f0cc4fb58f648bc00cbab2c0ca7bf8dbbf63d950
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2022-12-01 14:05:58 -0800

    Update 4.2.2.supervised_qpas.sh

--- 4.OHANA/4.2.2.supervised_qpas.sh
+++ 4.OHANA/4.2.2.supervised_qpas.sh
@@ -10,3 +10,9 @@ sed -i 's/CHB/7/' temp
 sed -i 's/JPT/8/' temp
 sed -i 's/YRI/9/' temp
 tr -d "\n\r" < temp | sed 's/./& /g' | fold -w80 >popasign.mat
+
+
+k=7 
+mi=450
+e=0.08
+qpas haenyeov143percent.dgm -k $k -qo supervised_v14_k${k}_e${e}_mi${mi}_q.matrix -fo supervised_v14_k${k}_e${e}_mi${mi}_f.matrix -e ${e} -mi $mi -fg fgk${k} > supervised.out.qpas${k}mi${mi}e${e} &
