3135b7b750b14097e2e3971891aa34bf4520efaf
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2022-12-05 21:16:58 -0500

    Update 4.5.selscan.sh

--- 4.OHANA/4.5.selscan.sh
+++ 4.OHANA/4.5.selscan.sh
@@ -10,7 +10,7 @@ done
 $OHANA/qpas haenyeov14.dgm -k 6 -qi heanyeo_v14_k6_e0.08_mi450_q.matrix -fo haenyeov14_k6.full.f.matrix -e 0.08 -mi 450 >out.qpas.full &
 
 #Run selscan
-for k in $(seq 0 6);
+for k in $(seq 0 5);
 do 
 echo "running selscan with selection in component k $k"
 $OHANA/selscan haenyeov14.dgm haenyeov14_k6.full.f.matrix c.matrix_k6 -cs c.matrix_k6selK${k}_h10 > scan.haenyeok$k.txt &
