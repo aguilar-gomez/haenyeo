078eff36d79e11de4dd186362c8c0135984d3dc9
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2022-12-07 21:28:10 -0300

    Update positionNameReduct.sh

--- 5.PlottingNCandidateSNP/positionNameReduct.sh
+++ 5.PlottingNCandidateSNP/positionNameReduct.sh
@@ -1,6 +1,6 @@
-#Add names of positions
-cat header_pos haenyeo.forohanav13.map > positions_selscan
-paste positions_selscan scan.haenyeok4.txt > selscan_k4_header.txt
+#Add names of positions k6
+cat header_pos haenyeo.forohanav14.map > positions_selscan
+paste positions_selscan scan.haenyeok3.txt > selscan_k3_header.txt
 
-#Filter by likelihood ratio
-awk '$6 > 2' selscan_k4_header.txt > smallscan_jejuComponent_k4.txt
+#Filter by likelihood ratio k6
+awk '$6 > 2' selscan_k3_header.txt > smallscan_jejuComponent_k3.txt
