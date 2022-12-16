745778026869589cf8f969bd7b4415ab370e4642
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2022-12-16 20:01:34 -0300

    Update positionNameReduct.sh

--- 5.PlottingNCandidateSNP/positionNameReduct.sh
+++ 5.PlottingNCandidateSNP/positionNameReduct.sh
@@ -4,3 +4,9 @@ paste positions_selscan scan.haenyeok3.txt > selscan_k3_header.txt
 
 #Filter by likelihood ratio k6
 awk '$6 > 2' selscan_k3_header.txt > smallscan_jejuComponent_k3.txt
+
+
+#Supervised k7 
+paste positions_selscan supervisedk7_scan.haenyeok3.txt > supervisedk7_selscan_k3_header.txt
+#Filter by likelihood ratio k7
+awk '$6 > 2' supervisedk7_selscan_k3_header.txt  > small_supervisedk7_scan.haenyeok3.txt 
