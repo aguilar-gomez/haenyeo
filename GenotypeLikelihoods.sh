a700f07c3dc7ea870868a57763db3b0b572df173
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2023-12-07 16:11:26 -0800

    Update GenotypeLikelihoods.sh

--- GenotypeLikelihoods.sh
+++ GenotypeLikelihoods.sh
@@ -7,3 +7,5 @@ echo $ind
 wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/data_collections/1000_genomes_project/data/CHS/$ind/alignment/$ind.alt_bwamem_GRCh38DH.20150718.CHS.low_coverage.cram{,.crai};
 done < CHSindividuals
 
+
+
