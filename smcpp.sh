23fc139e53828da671ae37c0bb3652a9cfdca6bb
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2024-01-29 21:21:04 -0800

    Update smcpp.sh

--- smcpp.sh
+++ smcpp.sh
@@ -10,7 +10,7 @@ for i in {1..22};
 do
 chr=chr$i
 echo $chr
-smc++ vcf2smc -m ${MASK} -d ${D1} ${D2} KoreanJeju.vcf.gz KOR.${D1}.${D2}.$chr.smc.gz $chr \
+smc++ vcf2smc -d ${D1} ${D2} KoreanJeju.vcf.gz KOR.${D1}.${D2}.$chr.smc.gz $chr \
 KOR:KOREA1K-348,KOREA1K-175,KOREA1K-366,KOREA1K-45,KOREA1K-70,KOREA1K-97,KOREA1K-30,KOREA1K-217
 done
 #####################################################################################
@@ -29,7 +29,7 @@ for i in {1..22};
 do
 chr=chr$i
 echo $chr
-smc++ vcf2smc -m ${MASK} -d ${D1} ${D2} KoreanJeju.vcf.gz JEJ.${D1}.${D2}.$chr.smc.gz $chr \
+smc++ vcf2smc -d ${D1} ${D2} KoreanJeju.vcf.gz JEJ.${D1}.${D2}.$chr.smc.gz $chr \
 JEJ:10_S44_L001_chr.bam,1_S35_L001_chr.bam,20_S53_L001_chr.bam
 done
 #####################################################################################
@@ -37,3 +37,38 @@ nohup bash Jej_vcf2smc.sh > outJEJconversionSub &
 
 
 smc++ estimate -o JEJ 1.25e-8 -p 0.5 JEJ.*.smc.gz
+
+
+#####################################################################################
+#KorJej_vcf2smc.sh
+#Convert vcf for each chromosome
+D1=KOREA1K-348
+D2=10_S44_L001_chr.bam
+for i in {1..22}; 
+do
+chr=chr$i
+echo $chr
+smc++ vcf2smc -d ${D1} ${D2} KoreanJeju.vcf.gz KORJEJ.${D1}.${D2}.$chr.smc.gz $chr \
+KOR:KOREA1K-348,KOREA1K-175,KOREA1K-366,KOREA1K-45,KOREA1K-70,KOREA1K-97,KOREA1K-30,KOREA1K-217 \
+JEJ:10_S44_L001_chr.bam,1_S35_L001_chr.bam,20_S53_L001_chr.bam
+done
+#####################################################################################
+nohup bash KorJej_vcf2smc.sh > outKORJEJconversionSub &
+
+
+#####################################################################################
+#JejKor_vcf2smc.sh
+#Convert vcf for each chromosome
+D1=10_S44_L001_chr.bam
+D2=KOREA1K-348
+for i in {1..22}; 
+do
+chr=chr$i
+echo $chr
+smc++ vcf2smc -d ${D1} ${D2} KoreanJeju.vcf.gz JEJKOR.${D1}.${D2}.$chr.smc.gz $chr \
+JEJ:10_S44_L001_chr.bam,1_S35_L001_chr.bam,20_S53_L001_chr.bam \
+KOR:KOREA1K-348,KOREA1K-175,KOREA1K-366,KOREA1K-45,KOREA1K-70,KOREA1K-97,KOREA1K-30,KOREA1K-217 
+
+done
+#####################################################################################
+nohup bash JejKor_vcf2smc.sh > outJEJKORconversionSub &
