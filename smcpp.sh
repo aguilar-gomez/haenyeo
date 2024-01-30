17299fd834897b21cb05b61ddcedb8c5b9988fba
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2024-01-29 19:39:48 -0800

    Update smcpp.sh

--- smcpp.sh
+++ smcpp.sh
@@ -15,3 +15,25 @@ KOR:KOREA1K-348,KOREA1K-175,KOREA1K-366,KOREA1K-45,KOREA1K-70,KOREA1K-97,KOREA1K
 done
 #####################################################################################
 nohup bash Kor_vcf2smc.sh > outKORconversionSub &
+
+smc++ estimate -o KOR 1.25e-8 -p 0.5 KOR.*.smc.gz
+
+
+#####################################################################################
+#Jej_vcf2smc.sh
+#Convert vcf for each chromosome
+D1=10_S44_L001_chr.bam
+D2=1_S35_L001_chr.bam
+MASK=20160622.allChr.mask.bed.gz
+for i in {1..22}; 
+do
+chr=chr$i
+echo $chr
+smc++ vcf2smc -m ${MASK} -d ${D1} ${D2} KoreanJeju.vcf.gz JEJ.${D1}.${D2}.$chr.smc.gz $chr \
+JEJ:10_S44_L001_chr.bam,1_S35_L001_chr.bam,20_S53_L001_chr.bam
+done
+#####################################################################################
+nohup bash Jej_vcf2smc.sh > outJEJconversionSub &
+
+
+smc++ estimate -o JEJ 1.25e-8 -p 0.5 JEJ.*.smc.gz
