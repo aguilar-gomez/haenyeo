4a92adace44ff688a9739a7cf3d344d0b0adb52a
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2024-02-09 16:58:07 -0800

    Update generateFrq.sh

--- 8.DivergenceTime/generateFrq.sh
+++ 8.DivergenceTime/generateFrq.sh
@@ -1,8 +1,4 @@
-/space/s2/diana/korea/haenyeo/Imputation2023/AnalysisNew/allPops
-
-Convert to format to use in Admixture Bayes
-Remove YRI samples, because of Neanderthal introgression mess
-
+#Convert to format to use in Treemix and Admixture Bayes
 awk '{ print $1, $2, $1 }' OFS="\t"  allchrfilteredAllMaf1.fam > pops.clust
 
 #Convert SEO,KOA and KMJ to KOR
@@ -14,12 +10,11 @@ sed -i 's/KMJ/KOR/g' pops.clust
 sed -i 's/JEU/JEJ/g' pops.clust
 sed -i 's/HAE/JEJ/g' pops.clust
 
+#Remove YRI samples, because of Neanderthal introgression mess
 grep "YRI" allchrfilteredAllMaf1.fam > YRI
 plink --bfile allchrfilteredAllMaf1 --geno 0 --out haeProject --double-id --remove-fam YRI --make-bed
-5673091 variants and 412 people pass filters and QC.
 
+#Generate frq file
 plink --bfile haeProject --geno 0 --freq --missing --within  pops.clust --out haeProject --double-id --remove-fam YRI 
-
 gzip haeProject.frq.strat
-
 plink2treemix.py haeProject.frq.strat.gz haeProject.treemix.frq.gz
