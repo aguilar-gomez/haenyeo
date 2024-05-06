c4c42e85dff27dc2dd44ac3d8bff260358dca89f
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2024-05-05 21:32:14 -0700

    Update 5.6pbs.sh

--- 5.StructureAndSelection/5.6pbs.sh
+++ 5.StructureAndSelection/5.6pbs.sh
@@ -1,89 +1,37 @@
 #PBS scan
 /space/s2/diana/korea/haenyeo/Imputation2023/AnalysisNew/outgroupCHS_allSamples/PBS
-cp ../allPopsUpdated.fam .
+cut -f1 -d " " selscan3popsCHSv2.fam|sort |uniq -c
+     53 CHS
+     51 JEJ
+    108 KOR
 
-grep "HAE\|JEU\|SEO\|CHB\|KOA" allPopsUpdated.fam |cut -f1|sort|uniq -c
-     57 CHB
-     27 HAE
-     24 JEU
-     77 KOA
-     26 SEO
-
-grep "HAE\|JEU\|SEO\|CHB\|KOA" allPopsUpdated.fam > samples4scans
-cut -f2 samples4scans > inds2keep
-sed 's/ /\t/g' pcapops.allchr.maf2.v11.fam > tab.fam
-
-
-filterbycol.py tab.fam 1 inds2keep inds2keep.fam
-
-Generate vcf
+#Generate vcf
 #Extract only Korean and Northern Chinese, keep sites variable in Korean 
-plink --file haenyeo.forohanav11 --recode vcf-iid --out selection_scansv11 --keep inds2keep.fam –extract onlyKor.bim
-nohup sh extract_for_scan.sh > out.extract4scan &
-6377716 variants and 206 people pass filters and QC.
+plink --file selscan3popsCHSv2 --recode vcf-iid --out selection_scans 
+5673091 variants and 212 people pass filters and QC.
 
+grep "JEJ" selscan3popsCHSv2.fam |cut -f2 -d " " > jej.list
+grep "KOR" selscan3popsCHSv2.fam |cut -f2 -d " " > kor.list
+grep "CHS" selscan3popsCHSv2.fam |cut -f2 -d " " > chs.list
 
-selection_scansv11
+#A jeju, seoul 
+vcftools --vcf selection_scans.vcf  \
+--weir-fst-pop jej.list --weir-fst-pop kor.list \
+--out ./jej_kor > fst.outA 
 
-At least 75% of the data (this did not work)
-vcftools --recode --vcf selection_scansv11.vcf --max-missing .25 --out selection_scansv11_reduced.vcf
-After filtering, kept 6377689 out of a possible 6377716 Sites
-grep "chr7_155206150" selection_scansv11_reduced.vcf.recode.vcf
-
-Max missing genotypes set to 30 (this is version12)
-vcftools --recode --vcf selection_scansv11.vcf --max-missing-count 30 --out selection_scansv11_missingCount
-Parameters as interpreted:
-    --vcf selection_scansv11.vcf
-    --max-missing-count 30
-    --out selection_scansv11_missingCount
-    --recode
-After filtering, kept 5,006,988 out of a possible 6,377,716 Sites
-
-grep "HAE"  samples4scans |cut -f2 >hae.list
-grep "JEJ"  samples4scans |cut -f2 >jej.list
-grep "SEO\|KOR"  samples4scans |cut -f2 >seo.list
-grep "CHB"  samples4scans |cut -f2 >chb.list
-grep "HAE\|JEJ"  samples4scans |cut -f2 >jisland.list
-cat hae.list jeju.list >haejeju.list
-
-VCFtools (0.1.17)
-Warning: Expected at least 2 parts in INFO entry: ID=PR,Number=0,Type=Flag,Description="Provisional reference allele, may not be based on real reference genome">
-
-#A heanyeo+jeju, seul 
-vcftools --vcf selection_scansv11.vcf  \
---weir-fst-pop haejeju.list --weir-fst-pop seo.list \
---fst-window-size 20000 --out ./haejeju_seul_20k > fst.w.outA 
-
-
-#B heanyeo+jeju, chinese
-vcftools --vcf selection_scansv11.vcf  \
---weir-fst-pop haejeju.list --weir-fst-pop chb.list \
---fst-window-size 20000 --out ./haejeju_chinese_20k > fst.w.outB 
+#B jeju, chinese
+vcftools --vcf selection_scans.vcf  \
+--weir-fst-pop jej.list --weir-fst-pop chs.list \
+--out ./jej_chinese > fst.outB 
 
 #C seul, chinese
-vcftools --vcf selection_scansv11.vcf  \
---weir-fst-pop seo.list --weir-fst-pop chb.list \
---fst-window-size 20000 --out ./seoul_chinese_20k > fst.w.outC 
+vcftools --vcf selection_scans.vcf  \
+--weir-fst-pop kor.list --weir-fst-pop chs.list \
+--out ./kor_chinese > fst.outC 
 
-By site
-#A heanyeo+jeju, seul 
-vcftools --vcf selection_scansv11.vcf  \
---weir-fst-pop haejeju.list --weir-fst-pop seo.list \
---out ./haejeju_seoul > fst.outA 
+paste -d "\t" jej_kor.weir.fst  jej_chinese.weir.fst kor_chinese.weir.fst | cut -f1-3,6,9 |grep -v "nan" > JEJKORCHB4pbs
 
-#B heanyeo+jeju, chinese
-vcftools --vcf selection_scansv11.vcf  \
---weir-fst-pop haejeju.list --weir-fst-pop chb.list \
---out ./haejeju_chinese > fst.outB 
 
-#C seul, chinese
-vcftools --vcf selection_scansv11.vcf  \
---weir-fst-pop seo.list --weir-fst-pop chb.list \
---out ./seoul_chinese > fst.outC 
-
-paste -d "\t" haejeju_seoul.weir.fst  haejeju_chinese.weir.fst seoul_chinese.weir.fst | cut -f1-3,6,9 |grep -v "nan" > hsc4pbs
-
-grep -v "nan" hsc.fst > hsc4pbs
 
 
 bedtools intersect -wa -wb -a HAE_JEJU_pbs_outliers_top_persitepbs1_25.bed -b annotation_grch38.bed |cut -f1-4,8>HAE_JEJU_pbs_anno25.bed
@@ -95,22 +43,6 @@ bedtools intersect -wa -wb -a HAE_JEJU_pbs_outliers_top_persite_chr6pbs1_25.bed 
 
 
 
-
-#By site
-#A heanyeo+jeju, seul
-vcftools --vcf selection_scansv11_missingCount.recode.vcf \
---weir-fst-pop haejeju.list --weir-fst-pop seo.list \
---out ./haejeju_seoulMC > fst.outAMC
-
-#B heanyeo+jeju, chinese
-vcftools --vcf selection_scansv11_missingCount.recode.vcf \
---weir-fst-pop haejeju.list --weir-fst-pop chb.list \
---out ./haejeju_chineseMC > fst.outBMC
-
-#C seul, chinese
-vcftools --vcf selection_scansv11_missingCount.recode.vcf \
---weir-fst-pop seo.list --weir-fst-pop chb.list \
---out ./seoul_chineseMC > fst.outCMC
 
 After filtering, kept 5006988 out of a possible 5006988 Sites
 
