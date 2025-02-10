565fff2048339dc1040dac76da63f68a489c3284
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2025-02-10 11:24:48 -0800

    Update 6.gemma.sh

--- 6.GeneticAssociation/6.gemma.sh
+++ 6.GeneticAssociation/6.gemma.sh
@@ -11,32 +11,5 @@ gemma -bfile assotest_haenyeov14 -gk 2 -o relatedness_matrix.haenyeo
 plink --bfile assotest_haenyeov14 --out assotest_haenyeov14_pos2test --extract SNP2test --make-bed
 plink --bfile assotest_haenyeov14 --out assotest_haenyeov14_allanno --extract posallanno --make-bed
 
-#Get covariate file
-cp /space/s2/diana/korea/haenyeo/June2022/plink/gemma/diving_Cov .
-
 #Run association
-gemma -bfile assotest_haenyeov14_pos2test -lmm 2 -o lrt_top10SNPs_diveCov -c diving_Cov -k output/relatedness_matrix.haenyeo.sXX.txt
-
-gemma -bfile assotest_haenyeov14_pos2test -lmm 2 -o lrt_top10SNPs_diveCov_forceSNPs -c diving_Cov -k output/relatedness_matrix.haenyeo.sXX.txt -notsnp -miss 1
-
-
-#Run with spleen size
-mkdir gemma_spleen
-#copy all files
-cp ../gemma_diastolic/assotest_haenyeov14* .
-cp ../gemma_diastolic/diving_Cov .
-
-#Replace phenotype file
-rm assotest_haenyeov14.fam
-cp spleen.fam assotest_haenyeov14.fam
-rm assotest_haenyeov14_pos2test.fam 
-cp spleen.fam assotest_haenyeov14_pos2test.fam
-
-
-#Run association
-gemma -bfile assotest_haenyeov14_pos2test -lmm 2 -o spleen_top10SNPs_diveCov -c diving_Cov -k ../gemma_diastolic/output/relatedness_matrix.haenyeo.sXX.txt
-gemma -bfile assotest_haenyeov14_allanno -lmm 2 -o spleen_posallanno_diveCov -c diving_Cov -k relatedness_matrix.haenyeo.sXX.txt
-gemma -bfile assotest_haenyeov14_allanno -lmm 2 -o diastolic_posallanno_diveCov -c diving_Cov -k relatedness_matrix.haenyeo.sXX.txt
-
-#No dive Cov
-gemma -bfile assotest_haenyeov14_pos2test -lmm 2 -o spleen_top10SNPs -k ../gemma_diastolic/output/relatedness_matrix.haenyeo.sXX.txt
+plink --bfile asso_haenyeo_pos2test --linear --covar covs_for_plink --out dias_untrans_top10 --allow-no-sex --hide-covar
